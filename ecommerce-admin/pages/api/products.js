8a292f4a559aca989df0778c5653941e1b9e64c7
je9rojas <je9rojas@gmail.com>
2025-02-11 23:02:14 -0500

    v1.2

--- ecommerce-admin/pages/api/products.js
+++ ecommerce-admin/pages/api/products.js
@@ -7,13 +7,17 @@ export default async function handle(req, res){
     await mongooseConnect();
 
     if(method == 'GET'){
-        res.json(await Product.find());
+        if(req.query?.id){
+            res.json(await Product.findOne({_id:req.query.id}));
+        } else {
+            res.json(await Product.find());
+        }        
     }
 
     if (method === 'POST'){
         const {title, description, price} = req.body;
         const productDoc = await Product.create({
-            title, description, price
+            title, description, price,
 
         })
         res.json(productDoc);
